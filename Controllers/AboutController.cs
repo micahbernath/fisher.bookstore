cd2b5fc9e6b1e406995ad7a2b538de793e2a7b60
micahbernath <bernath.13@osu.edu>
2018-01-24 18:35:22 -0500

    Added routing and controllers

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("about me");
+        }
+
+        public IActionResult Location()
+        {
+            return Content("the location page");
+        }
+
+        public IActionResult History()
+        {
+            return Content("history page");
+        }
+    }
+}
